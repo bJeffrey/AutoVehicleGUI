f38ef3b24ed42303438148e542c4a87afd433ee1
bJeffrey <noej@oregonstate.edu>
2018-06-25 17:45:06 -0700

    added a few buttons

--- AutoVehicle/src/MainFrame.java
+++ AutoVehicle/src/MainFrame.java
@@ -1,22 +1,31 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.GridLayout;
+//import java.awt.GridLayout;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.BorderLayout;
+//import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.BorderLayout;
 import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainFrame {
 
 	private JFrame frame;
-	private JTextField txtATextField;
+	private JTextField txtVehicleIp;
 
 	/**
 	 * Launch the application.
@@ -46,54 +55,90 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 12));
+		frame.setBackground(Color.LIGHT_GRAY);
 		frame.getContentPane().setBackground(Color.WHITE);
 		
-		JLabel lblNewLabel = new JLabel("Here I am");
-		lblNewLabel.setBackground(Color.LIGHT_GRAY);
+		JLabel lbllabelexample = new JLabel("label example");
+		lbllabelexample.setHorizontalAlignment(SwingConstants.CENTER);
+		lbllabelexample.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lbllabelexample.setOpaque(true);
+		lbllabelexample.setBackground(Color.LIGHT_GRAY);
+		lbllabelexample.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
+
+		txtVehicleIp = new JTextField();
+		txtVehicleIp.setText("Vehicle IP");
+		txtVehicleIp.setColumns(10);
 		
-		txtATextField = new JTextField();
-		txtATextField.setText("a text field");
-		txtATextField.setColumns(10);
+		JButton btnSearch = new JButton("Search");
+		btnSearch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String HOST = txtVehicleIp.getText();
+			}
+		});
+
+		JButton btnFwd = new JButton("fwd");
+		btnFwd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		
-		JButton btnNewButton = new JButton("anchored");
+		JButton btnLeft = new JButton("left");
 		
-		JButton btnAnchored = new JButton("anchored");
+		JButton btnRight = new JButton("right");
+		
+		JButton btnBack = new JButton("back");
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(601)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(130)
-							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 132, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(99)
-							.addComponent(txtATextField, GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(161)
-							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 54, Short.MAX_VALUE)))
-					.addGap(265))
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(327, Short.MAX_VALUE)
-					.addComponent(btnAnchored, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
-					.addGap(153))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnLeft)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnRight))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(28)
+									.addComponent(btnFwd)))
+							.addPreferredGap(ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+							.addComponent(lbllabelexample, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addComponent(txtVehicleIp, GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+							.addGap(18)
+							.addComponent(btnSearch, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)))
+					.addGap(119))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(629, Short.MAX_VALUE)
+					.addComponent(btnBack)
+					.addGap(302))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(146)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(txtATextField, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-					.addGap(40)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addGap(458)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnSearch, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtVehicleIp, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
-					.addComponent(btnAnchored, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(35, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lbllabelexample, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(btnFwd)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnRight)
+								.addComponent(btnLeft))))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnBack)
+					.addContainerGap(74, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
-		frame.setBounds(100, 100, 603, 431);
+		frame.setBounds(100, 100, 1000, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
